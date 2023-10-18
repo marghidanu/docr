3f40920b58f3b1fbf23121bef7334e1e4d71f3b1
Tudor Marghidanu <tudor@f2pool.io>
2023-10-18 21:41:37 +0300

    Refactored container tests

--- spec/containers_spec.cr
+++ spec/containers_spec.cr
@@ -1,33 +1,43 @@
 require "./spec_helper"
 
-client = Docr::Client.new
-api = Docr::API.new(client)
+def setup
+  client = Docr::Client.new
+  api = Docr::API.new(client)
+  api.images.create("nginx", "latest")
 
-api.images.create("nginx", "latest")
+  api
+end
 
 describe "Containers" do
   it "should create a new container" do
+    api = setup
+
     config = Docr::Types::CreateContainerConfig.new(
       image: "nginx:latest",
       host_config: Docr::Types::HostConfig.new,
     )
 
-    _ = api.containers.create("abc", config)
+    api.containers.create("abc", config)
     api.containers.start("abc")
   end
 
   it "should inspect container" do
-    result = api.containers.inspect("abc")
+    api = setup
 
+    result = api.containers.inspect("abc")
     result.should_not be_nil
     result.name.should eq "/abc"
   end
 
   it "should display running processes inside the container" do
-    _ = api.containers.top(id: "abc", ps_args: "aux")
+    api = setup
+
+    api.containers.top(id: "abc", ps_args: "aux")
   end
 
   it "should list all containers" do
+    api = setup
+
     result = api.containers.list(
       limit: 1,
       filters: {"name" => ["abc"]},
@@ -40,6 +50,8 @@ describe "Containers" do
   end
 
   it "should delete the container" do
+    api = setup
+
     api.containers.stop("abc")
     api.containers.delete("abc")
 
