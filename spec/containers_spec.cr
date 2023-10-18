bc014eac20182cbecff7592864bb2c407a9af677
Tudor Marghidanu <tudor@f2pool.io>
2023-10-18 21:43:15 +0300

    Reverted container tests

--- spec/containers_spec.cr
+++ spec/containers_spec.cr
@@ -1,43 +1,33 @@
 require "./spec_helper"
 
-def setup
-  client = Docr::Client.new
-  api = Docr::API.new(client)
-  api.images.create("nginx", "latest")
+client = Docr::Client.new
+api = Docr::API.new(client)
 
-  api
-end
+api.images.create("nginx", "latest")
 
 describe "Containers" do
   it "should create a new container" do
-    api = setup
-
     config = Docr::Types::CreateContainerConfig.new(
       image: "nginx:latest",
       host_config: Docr::Types::HostConfig.new,
     )
 
-    api.containers.create("abc", config)
+    _ = api.containers.create("abc", config)
     api.containers.start("abc")
   end
 
   it "should inspect container" do
-    api = setup
-
     result = api.containers.inspect("abc")
+
     result.should_not be_nil
     result.name.should eq "/abc"
   end
 
   it "should display running processes inside the container" do
-    api = setup
-
-    api.containers.top(id: "abc", ps_args: "aux")
+    _ = api.containers.top(id: "abc", ps_args: "aux")
   end
 
   it "should list all containers" do
-    api = setup
-
     result = api.containers.list(
       limit: 1,
       filters: {"name" => ["abc"]},
@@ -50,8 +40,6 @@ describe "Containers" do
   end
 
   it "should delete the container" do
-    api = setup
-
     api.containers.stop("abc")
     api.containers.delete("abc")
 
