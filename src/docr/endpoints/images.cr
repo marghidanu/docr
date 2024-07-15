a2f8f4e77fa0cc72a03f6828e814a32b375508ad
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-15 00:01:14 -0300

    Pass version argument in image build

--- src/docr/endpoints/images.cr
+++ src/docr/endpoints/images.cr
@@ -93,6 +93,7 @@ module Docr::Endpoints
         "platform"   => platform,
         "target"     => target,
         "outputs"    => outputs,
+        "version"    => version,
       }
 
       params["extrahosts"] = extra_hosts if extra_hosts
